11a1738e3e61f4bbca383ed2e60af71955b4238d
Affealshexer <152151583+Affealshexer@users.noreply.github.com>
2024-12-12 18:40:05 +0100

    Update Spielfeld.py

--- Spielfeld.py
+++ Spielfeld.py
@@ -17,23 +17,17 @@ class Spielfeld():
 
     def Effekte(self):
         """Definiert die Effekte"""
-        self.E = {
-            "E1": "",
-            "E2": "",
-            "E3": "",
-            "E4": "",
-            "E5": "",
-            "E6": ""}
+        self.E = []
     
-    def gibEffekt(self, k):
+    def verteileEffekt(self, k):
         """Gibt einen Effekt zurueck"""
         return self.E[k]
 
     def randomEffekt(self):
         """Gibt einen zufaelligen Effekt zurueck"""
-        return self.gibEffekt[f"E{randint(1,len(self.E))}"]
+        return self.verteileEffekt[randint(0,len(self.E))]
 
-    def randomEffekteVerteilen(self, n=0):
+    def randomEffekteErstellen(self, n=0):
         """Erstellt zufaellige Effekte fuer das Spielfeld und fuegt sie ein"""
         if not n:
             n = randint(0,len(self.SFeld)-1)
@@ -42,8 +36,7 @@ class Spielfeld():
             r = randint(0,len(self.SFeld)-1)
             if not any(r in tupel for tupel in l):
                 l += (r, self.randomEffekt())
-        
-        self.gibEffekte(l)
+        self.verteileEffekte(l)
                 
                 
         
