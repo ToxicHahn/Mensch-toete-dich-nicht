8c4007149233de3a4acd835ad12862fb4e310fac
adrian.grieger@proton.me <adrian.grieger@proton.me>
2024-12-12 14:13:14 +0100

    added comments for doc

--- Feld.py
+++ Feld.py
@@ -1,14 +1,21 @@
 class Feld():
     def __init__(self, effekt=None):
         self.effekt = effekt
-        self.besetzt = False
+        self.besetzt: bool = False
     def setzeEffekt(self, effekt):
+        """Setzt einen Effekt fest"""
         self.effekt = effekt
     def holeEffekt(self):
+        """Returned den aktuell gesetzten Effekt"""
         return self.effekt
     def istBesetzt(self):
+        """Returned Besetzung Status
+         False = Leeres Feld
+         True = Besetzes Feld"""
         return self.besetzt
-    def feldBesetzen(self):
+    def besetzen(self):
+        """Setzt dieses Feld auf besetzt"""
         self.besetzt = True
-    def feldVerlassen(self):
+    def verlassen(self):
+        """Setzt dieses Feld auf leer"""
         self.besetzt = False
