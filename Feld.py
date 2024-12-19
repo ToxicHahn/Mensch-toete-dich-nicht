1d8452f409e073467c745452654610e427479382
lilobatman <liloemilio2@gmail.com>
2024-12-19 18:13:10 +0100

    Add files via upload

--- Feld.py
+++ Feld.py
@@ -1,21 +1,48 @@
 class Feld():
-    def __init__(self, effekt=None):
-        self.effekt = effekt
-        self.besetzt: bool = False
+    def __init__(self, nummer, position, ist_start=False, ist_ziel=False, ):
+        self.nummer = nummer
+        self.position = position
+        self.ist_start = ist_start
+        self.ist_ziel = ist_ziel
+        self.besetztVon = None  # Figur, die sich aktuell auf diesem Feld befindet
+
+    def istFrei(self):
+        return self.besetztVon is None
+
+    def betreten(self, figur):
+        if self.istFrei():
+            self.besetztVon = figur
+            figur.position = self.position
+            return True
+        return False
+
+    def verlassen(self):
+        self.besetztVon = None
+
+    def setzePosition(self, position):
+        self.position = position
+
+    def holePosition(self):
+        return self.position
+        
     def setzeEffekt(self, effekt):
         """Setzt einen Effekt fest"""
         self.effekt = effekt
+        
     def holeEffekt(self):
         """Returned den aktuell gesetzten Effekt"""
         return self.effekt
+    
     def istBesetzt(self):
         """Returned Besetzung Status
          False = Leeres Feld
          True = Besetzes Feld"""
         return self.besetzt
+    
     def besetzen(self):
         """Setzt dieses Feld auf besetzt"""
         self.besetzt = True
+        
     def verlassen(self):
         """Setzt dieses Feld auf leer"""
         self.besetzt = False
--- Spielfeld.py
+++ Spielfeld.py
@@ -1,17 +1,96 @@
 from Feld import *
+from Farbfeld import *
+from Figur import *
+from Wuerfel import *
 from random import * 
 
 class Spielfeld():
-    def __init__(self, n: int):
-        def erstelleFeld(self, n: int):
-            """Erstellt ein Feld der geforderten Groesse"""
-            for i in range(n):
-                self._SFeld.append(Feld())
+    def __init__(self):
+        self._SFeld = []  # Hauptspielfeld
+        self.startFeld = []
+        self.zielFeld = []
 
-        self._SFeld = []
-        erstelleFeld(n)
+        self.startfelder = {
+            "rot": [(0,0),(0,1),(1,0),(1,1)],
+            "blau": [(0,9),(0,10),(1,9),(1,10)],
+            "gelb": [(9,0),(9,1),(10,0),(10,1)],
+            "gruen": [(9,9),(9,10),(10,9),(10,10)],
+        }  # Startfelder pro Farbe
+
+        self.zielfelder = {
+            "rot": [(5,1),(5,2),(5,3),(5,4)],
+            "blau": [(1,5),(2,5),(3,5),(4,5)],
+            "gelb": [(6,5),(7,5),(8,5),(9,5)],
+            "gruen": [(5,6),(5,7),(5,8),(5,9)],
+        }  # Zielfelder pro Farbe
+
+        self.figuren = {
+            "rot": [Figur("rot", i) for i in range(4)],
+            "blau": [Figur("blau", i) for i in range(4)],
+            "gelb": [Figur("gelb", i) for i in range(4)],
+            "gruen": [Figur("gruen", i) for i in range(4)],
+        }
+
+        #print(len(self.felder))
+
+        # for farbe in ["rot", "blau", "gelb", "gruen"]:
+        #     self.startfelder[farbe] = [Farbfeld(i, farbe, ist_start=True) for i in range(4)]
+        #     self.zielfelder[farbe] = [Farbfeld(i, farbe, ist_ziel=True) for i in range(4)]
+
+    def holeFeld(self, nummer):
+        pass
     
+    def bewegeFigur(self, figur, spalte, reihe):
+        """Platziert Figuren auf ihren Startfeldern"""
+        for felder in self._SFeld:
+            if felder.position == (reihe, spalte): 
+                feld = felder   # Position des gewollten Feldes
 
+        if feld.besetzt is None:  # Nur platzieren, wenn das Feld leer ist
+            feld.besetztVon = figur  # Figur auf das Feld setzen
+            if not figur.imZielFrage:
+                figur.setzePosition((spalte, reihe))  # Position der Figur aktualisieren
+        else:
+            print("Feld ist bereits besetzt!")
+
+    def erstelleFeld(self, n: int):
+        """Erstellt ein Feld der geforderten Groesse"""
+        for i in range(n):
+            self._SFeld.append(Feld(i+1, None))
+
+        """Definiert die Pfadfelder (Positionen der weißen Kreise)"""
+        pfad_coords = [
+            (0,4), (0,5), (0,6), (1,4), (1,6),
+            (2,4), (2,6), (3,4), (3,6), (4,0),
+            (4,1), (4,2), (4,3), (4,4), 
+            (4,6), (4,7), (4,8), (4,9),
+            (4,10), (5,0), (5,10), (6,0),
+            (6,1), (6,2), (6,3), (6,4),
+            (6,6), (6,7), (6,8), (6,9), (6,10),
+            (7,4), (7,6), (8,4), (8,6),
+            (9,4), (9,6), (10,4), (10,5), (10,6)
+        ]
+
+        for i, feld in enumerate(self._SFeld):
+            if i < len(pfad_coords):
+                reihe, spalte = pfad_coords[i]
+                feld.setzePosition((reihe, spalte))
+
+        farben = ["rot", "blau", "gelb", "gruen"]
+
+        for farbe in farben:
+            # Startfelder
+            for position in self.startfelder[farbe]:
+                farbfeld = Farbfeld(farbe, (position[0],position[1]), ist_start=True)
+                # self.startFeld.append(farbfeld)
+                self._SFeld.append(farbfeld)
+
+            # Zielfelder
+            for position in self.zielfelder[farbe]:
+                farbfeld = Farbfeld(farbe, (position[0], position[1]), ist_ziel=True)
+                # self.zielFeld.append(farbfeld)
+                self._SFeld.append(farbfeld)
+   
     
     def EffekteEinfuegen(self, l: list[tuple[int, str]]):
         """Fuegt die Effekte in die Felder des Spielfelds ein"""
@@ -33,10 +112,10 @@ class Spielfeld():
     def randomEffekteErstellen(self, n: int = 0):
         """Erstellt zufaellige Effekte fuer das Spielfeld und fuegt sie ein"""
         if not n:
-            n = randint(0,len(self.SFeld)-1)
+            n = randint(0,len(self._SFeld)-1)
         l = []
         for i in range(n):
-            r = randint(0,len(self.SFeld)-1)
+            r = randint(0,len(self._SFeld)-1)
             if not any(r in tupel for tupel in l):
                 l += (r, self.randomEffekt())
         self.EffekteEinfuegen(l)
